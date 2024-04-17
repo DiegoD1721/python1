7b71cf5e5f5844550a37c4c14a43c82bea5c641c
DiegoD1721 <148830538+DiegoD1721@users.noreply.github.com>
2024-04-17 14:42:22 -0400

    Add files via upload

---
+++ zoo.py
@@ -0,0 +1,16 @@
+def hours():
+    print('Open 9-5 daily')
+
+import zoo
+zoo.hours()
+
+
+import zoo as menagerie
+menagerie.hours()
+
+from zoo import hours
+hours()
+
+
+from zoo import hours as info
+info()
